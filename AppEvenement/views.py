c24901b7f3f0068797e31f5a59403ec2f7a2b0ed
Mickael004 <144129482+Mickael004@users.noreply.github.com>
2025-06-18 15:50:40 +0300

    views creer evenement effectuer

--- AppEvenement/views.py
+++ AppEvenement/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import *
 from django.utils import timezone
+from AppMembre.models import Utilisateur
 
 # Create your views here.
 
@@ -28,3 +30,60 @@ def detail_evenement(request,form_id):
 
     return render(request,'DetailEvenement.html')
 
+# insertion imqge
+def inserer_photo(request,titre):
+    if request.FILES.get("photo"):
+        image = request.FILES.get("image")
+        #Enregistrement avec nom unique
+        with open(f"static/images/evenements/{titre}.jpg","wb") as destination:
+            for chunk in image.chunks():
+                destination.write(chunk)
+        return f"{titre}.jpg"
+    
+
+# creer Evenement
+def creer_evenement(request):
+    if request.session.get('membres'):
+        if request.session['membres']['role'] in ['administrateur','moderateur']:
+            if request.method == 'POST':
+                titre = request.POST.get('nom')
+                description = request.POST.get('description')
+                date_debut = request.POST.get('date_debut')
+                date_fin = request.POST.get('date_fin')
+                lieu = request.POST.get('lieu')
+                image = request.POST.get('image')
+                participants = request.POST.get('participants')
+                createur = request.session['membres']['id']
+
+                statut = request.POST.get('statut')
+                # nom imqge
+                nom_image = f"{titre}_{date_debut}"
+
+                evenements = Evenement(
+                    titre=titre,
+                    description=description,
+                    date_debut=date_debut,
+                    date_fin=date_fin,
+                    lieu=lieu,
+                    image=f"static/images/evenemets/{inserer_photo(request,nom_image)}",
+                    statut = statut,
+                    createur=createur,
+                    participants=participants
+                )
+                evenements.save()
+                messages.success(request,'Evenement Creer avec succes')
+                return redirect('listEvenement')
+
+        else:
+            messages.error(request, "Accès non autorisé.")
+            return redirect('accueil')
+
+
+
+# Participation Evenement
+
+def participer_evenement(request,form_id):
+    evenement = get_object_or_404(Evenement, form_id=form_id)
+
+    if evenement.participants.filter(id=request.session['membres']['id']).exists():
+        evenement.participants.remove(Utilisateur.id)
