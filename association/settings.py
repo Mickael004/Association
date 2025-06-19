da122d3d8dffa48f0e7da9bfb0a445971856774f
Mickael004 <144129482+Mickael004@users.noreply.github.com>
2025-06-19 07:54:37 +0300

    avan la remodification Evenement

--- association/settings.py
+++ association/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'AppAccueil',
     'AppMembre',
-    'AppEvenement',
+    # 'AppEvenement',
     'AppActualite',
     'AppBlog',
     'AppMessagerie',
